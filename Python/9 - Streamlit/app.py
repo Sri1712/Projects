c72ebfe6721608a6dc8a9c0df6d67e9be2b8f9e1
Srilathaa Vasu <sripriyavasu2004@gmail.com>
2026-01-29 12:40:16 +0100

    Streamlit widgets

--- Python/9 - Streamlit/app.py
+++ Python/9 - Streamlit/app.py
@@ -4,3 +4,22 @@ import numpy as np
 
 ## Title
 st.title("Hello Streamlit")
+
+#Display a text
+st.write("Display a simple text")
+
+#create a dataframe
+df = pd.DataFrame({
+    'Column 1': np.random.randn(10),
+    'Column 2': np.random.randn(10)
+})
+
+#Display the dataframe
+st.dataframe(df)
+
+#Create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+st.line_chart(chart_data)
